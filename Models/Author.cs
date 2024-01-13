60cd7bcffa8b75a9d6fee7032aa7d81e2af3b883
KamilDoniek <kamil.doniek@microsoft.wsei.edu.pl>
2024-01-13 18:54:58 +0100

    Walidacja

--- Models/Author.cs
+++ Models/Author.cs
@@ -7,8 +7,10 @@ public class Author
     public int AuthorId { get; set; }
     
     [Required(ErrorMessage = "Imię autora jest wymagane.")]
+    [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Imie autora może zawierać tylko litery.")]
     public string FirstName { get; set; }
     [Required(ErrorMessage = "Nazwisko autora jest wymagane.")]
+    [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Nazwisko autora może zawierać tylko litery.")]
 
     public string LastName { get; set; }
 
--- Models/User.cs
+++ Models/User.cs
@@ -7,10 +7,10 @@ public class User
 {
     public int UserId { get; set; }
     [Required(ErrorMessage = "Imię użytkownika jest wymagane.")]
-
+    [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Imie użytkowanika może zawierać tylko litery.")]
     public string FirstName { get; set; }
     [Required(ErrorMessage = "Nazwisko użytkownika jest wymagane.")]
-
+    [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Nazwisko użytkowania może zawierać tylko litery.")]
     public string LastName { get; set; }
     [Required(ErrorMessage = "Adres email użytkownika jest wymagany.")]
     [EmailAddress(ErrorMessage = "Nieprawidłowy format adresu email.")]
